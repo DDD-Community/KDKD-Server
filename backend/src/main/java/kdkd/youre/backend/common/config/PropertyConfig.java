06b6c445ce2e0e415c2ae173fac71c4965ae87e6
Choo <choo000407@naver.com>
2023-06-12 23:54:30 +0900

    Add: 초기 환경 설정, DB 연결
    env.properties 추가 필요

---
+++ backend/src/main/java/kdkd/youre/backend/common/config/PropertyConfig.java
@@ -0,0 +1,12 @@
+package kdkd.youre.backend.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:env.properties")
+})
+public class PropertyConfig {
+}
