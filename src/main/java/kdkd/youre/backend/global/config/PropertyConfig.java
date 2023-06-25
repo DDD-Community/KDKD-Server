93d95b863948a16a6259e0aae4e45969c0d39b40
Choo <choo000407@naver.com>
2023-06-25 23:37:44 +0900

    Rename: 패키지명 변경

--- src/main/java/kdkd/youre/backend/global/config/PropertyConfig.java
+++ src/main/java/kdkd/youre/backend/global/config/PropertyConfig.java
@@ -1,4 +1,4 @@
-package kdkd.youre.backend.common.config;
+package kdkd.youre.backend.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
