b2d97f8e88d959637cf47ec6cb108d96e3c4620c
ChooSeoYeon <choo000407@naver.com>
2023-06-27 10:49:50 +0900

    Refactor: Builder로 id 지정 못하게 수정

--- src/main/java/kdkd/youre/backend/domain/member/repository/Member.java
+++ src/main/java/kdkd/youre/backend/domain/member/repository/Member.java
@@ -13,17 +13,15 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
-    private String nickname;
-    private String email;
-    private String oauthProvider;
+    private String nickname; // 회원 이름
+    private String email; // 회원 이메일
+    private String oauthProvider; // oAuth Provider (google)
 
     @Builder
     public Member(
-            Long id,
             String nickname,
             String email,
             String oauthProvider) {
-        this.id = id;
         this.nickname = nickname;
         this.email = email;
         this.oauthProvider = oauthProvider;
