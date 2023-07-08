7f723872855897b6cb857db87dcb4d75cc9dab8c
Choo <choo000407@naver.com>
2023-07-08 20:46:10 +0900

    Add: CORS 설정 추가

---
+++ src/main/java/kdkd/youre/backend/global/config/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package kdkd.youre.backend.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private final CorsConfig corsConfig;
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(corsConfig.corsFilter())
+                .authorizeRequests(authorize ->authorize
+                        .antMatchers("/").permitAll()
+                )
+                .build();
+    }
+}
