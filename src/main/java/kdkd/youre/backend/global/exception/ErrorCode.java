18fd3e58caed897d6f0808d4a8c9459d52fc2b58
Choo <choo000407@naver.com>
2023-07-08 20:34:20 +0900

    Add: ErrorCode enum 추가

---
+++ src/main/java/kdkd/youre/backend/global/exception/ErrorCode.java
@@ -0,0 +1,17 @@
+package kdkd.youre.backend.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    //공통 예외
+    BAD_REQUEST_PARAM(HttpStatus.BAD_REQUEST.BAD_REQUEST, "잘못된 요청입니다."),
+    BAD_REQUEST_VALIDATION(HttpStatus.BAD_REQUEST, "검증에 실패하였습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String detail;
+}
