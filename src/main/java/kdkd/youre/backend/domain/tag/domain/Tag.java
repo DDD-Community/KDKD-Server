f4ae4bd851c794a08c9a3cd694a6f17d8cb9c4d7
Choo <choo000407@naver.com>
2023-07-29 20:23:41 +0900

    Feat: Tag 도메인 구축 (#15)

---
+++ src/main/java/kdkd/youre/backend/domain/tag/domain/Tag.java
@@ -0,0 +1,32 @@
+package kdkd.youre.backend.domain.tag.domain;
+
+import kdkd.youre.backend.domain.url.domain.Url;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tag_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "url_id")
+    private Url url;
+
+    @Builder
+    public Tag(String name, Url url) {
+        this.name = name;
+        this.url = url;
+    }
+}
