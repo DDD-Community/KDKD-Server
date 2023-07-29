e97ab3a20b5b291240a25692c79955b612e3e90b
ChooSeoYeon <choo000407@naver.com>
2023-07-29 20:02:00 +0900

    Feat: 회원 정보 API에 인증 필터 적용

--- src/main/java/kdkd/youre/backend/domain/member/presentation/MemberController.java
+++ src/main/java/kdkd/youre/backend/domain/member/presentation/MemberController.java
@@ -2,10 +2,12 @@ package kdkd.youre.backend.domain.member.presentation;
 
 import kdkd.youre.backend.domain.member.service.MemberService;
 import kdkd.youre.backend.domain.member.presentation.dto.response.MemberFindResponse;
+import kdkd.youre.backend.global.security.auth.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +20,9 @@ public class MemberController {
 
     // 회원 정보 조회
     @GetMapping("/members")
-    public ResponseEntity<MemberFindResponse> findMember() {
+    public ResponseEntity<MemberFindResponse> findMember(@AuthenticationPrincipal PrincipalDetails principalDetails) {
 
-        MemberFindResponse response = memberService.findMember(1L);
+        MemberFindResponse response = memberService.findMember(principalDetails.getMember().getId());
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
