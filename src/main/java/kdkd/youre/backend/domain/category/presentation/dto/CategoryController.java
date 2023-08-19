aec0ae84aea79fae2e968ca1348d00e87a15f62d
ChoiSangOk <tkddhr12345@naver.com>
2023-08-19 14:16:30 +0900

    Feat: Category 저장 API 수정

--- src/main/java/kdkd/youre/backend/domain/category/presentation/dto/CategoryController.java
+++ src/main/java/kdkd/youre/backend/domain/category/presentation/dto/CategoryController.java
@@ -3,9 +3,11 @@ package kdkd.youre.backend.domain.category.presentation.dto;
 import kdkd.youre.backend.domain.category.presentation.dto.request.CategorySaveRequest;
 import kdkd.youre.backend.domain.category.service.CategoryService;
 import kdkd.youre.backend.domain.common.presentation.dto.response.IdResponse;
+import kdkd.youre.backend.global.security.auth.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +22,10 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @PostMapping("")
-    public ResponseEntity<IdResponse> saveCategory(@RequestBody CategorySaveRequest request) {
+    public ResponseEntity<IdResponse> saveCategory(@RequestBody CategorySaveRequest request,
+                                                   @AuthenticationPrincipal PrincipalDetails principalDetails) {
 
-        IdResponse response = categoryService.saveCategory(request);
+        IdResponse response = categoryService.saveCategory(request, principalDetails.getMember());
         return ResponseEntity.ok().body(response);
 //    return null;
     }
--- src/main/java/kdkd/youre/backend/domain/category/service/CategoryService.java
+++ src/main/java/kdkd/youre/backend/domain/category/service/CategoryService.java
@@ -4,6 +4,8 @@ import kdkd.youre.backend.domain.category.domain.Category;
 import kdkd.youre.backend.domain.category.domain.repository.CategoryRepository;
 import kdkd.youre.backend.domain.category.presentation.dto.request.CategorySaveRequest;
 import kdkd.youre.backend.domain.common.presentation.dto.response.IdResponse;
+import kdkd.youre.backend.domain.member.domain.Member;
+import kdkd.youre.backend.domain.url.service.UrlService;
 import kdkd.youre.backend.global.exception.CustomException;
 import kdkd.youre.backend.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -18,17 +20,23 @@ import org.springframework.transaction.annotation.Transactional;
 public class CategoryService {
     private final CategoryRepository categoryRepository;
 
-    public IdResponse saveCategory(CategorySaveRequest request) {
+    private final UrlService urlService;
+
+    public IdResponse saveCategory(CategorySaveRequest request, Member member) {
 
         //카테고리 중복체크
         Category category = categoryRepository.findByName(request.getName());
 
-        if (category != null) {
-            throw new CustomException(ErrorCode.CONFLICT_CATEGORY);
-        }
+        validateCategoryOwnerShip(category, member);
+
+//        if (category != null) {
+//            throw new CustomException(ErrorCode.CONFLICT_CATEGORY);
+//        }
 
         Category category1 = Category.builder()
                 .name(request.getName())
+                .member(member)
+                .parent(category)
                 .build();
 
         categoryRepository.save(category1);
@@ -36,5 +44,9 @@ public class CategoryService {
         return IdResponse.builder()
                 .id(category1.getId())
                 .build();
+    }
+    public void validateCategoryOwnerShip(Category category, Member member) { // TODO: 위치 혹은 이름 더 적절하게 변경하기
+        if (!category.isPublishedBy(member))
+            throw new CustomException(ErrorCode.FORBIDDEN_MEMBER);
     }
 }
