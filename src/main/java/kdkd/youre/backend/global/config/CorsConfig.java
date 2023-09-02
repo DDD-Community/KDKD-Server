aed535a1792a55ad0aee4010582a94d5e30d9bcf
ChooSeoYeon <choo000407@naver.com>
2023-09-02 17:31:50 +0900

    chore: chrome extension cors 설정 추가

--- src/main/java/kdkd/youre/backend/global/config/CorsConfig.java
+++ src/main/java/kdkd/youre/backend/global/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package kdkd.youre.backend.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,6 +10,9 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
+    @Value("chrome.extension.id")
+    private String extensionId;
+
     @Bean
     public CorsFilter corsFilter() {
 
@@ -17,6 +21,7 @@ public class CorsConfig {
 
         configuration.setAllowCredentials(true); // 내 서버가 응답을 할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정하는 것
         configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOrigin("chrome-extension://" + extensionId);
         configuration.addAllowedHeader("*"); // 모든 header에 응답을 허용
         configuration.addAllowedMethod("*"); // 모든 post, get, put, delete, patch 요청을 허용
 
