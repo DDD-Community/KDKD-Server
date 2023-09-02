68806eaed6895c5809de5b6b752052d8a41c7fdd
Choo <choo000407@naver.com>
2023-09-02 21:16:48 +0900

    Fix: Cascade 설정 넣어서 Url 삭제 API 에러 픽스 (#57)
    * Chore: ddl-auto update로 변경
    
    * Feat: baseTimeEntity 적용
    
    * Fix: cascade 제약조건 추가

--- src/main/java/kdkd/youre/backend/domain/url/domain/Url.java
+++ src/main/java/kdkd/youre/backend/domain/url/domain/Url.java
@@ -8,6 +8,8 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 
@@ -21,15 +23,16 @@ public class Url extends BaseTimeEntity {
     @Column(name = "url_id")
     private Long id;
 
-    private String urlAddress; // url 주소
-    private String name; //url 이름
-    private String thumbnail; // 썸네일 이미지
-    private String memo; // 메모
+    private String urlAddress;      // url 주소
+    private String name;            //url 이름
+    private String thumbnail;       // 썸네일 이미지
+    private String memo;            // 메모
     private Boolean isWatchedLater; // 나중에 보기 여부
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
-    private Category category; // category id
+    @OnDelete(action = OnDeleteAction.CASCADE) // Url은 Category 제거 시 함께 제거
+    private Category category;
 
     @Builder
     public Url(
