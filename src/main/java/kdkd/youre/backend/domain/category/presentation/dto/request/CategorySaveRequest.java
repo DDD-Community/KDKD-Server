5345f0929a56149572aaf0f7929e9106b5834ca7
ChooSeoYeon <choo000407@naver.com>
2023-09-02 16:51:19 +0900

    Refactor: 패키지 알맞게 수정

--- src/main/java/kdkd/youre/backend/domain/category/presentation/dto/request/CategorySaveRequest.java
+++ src/main/java/kdkd/youre/backend/domain/category/presentation/dto/request/CategorySaveRequest.java
@@ -2,11 +2,10 @@ package kdkd.youre.backend.domain.category.presentation.dto.request;
 
 import lombok.*;
 
-import java.util.List;
-
 @Getter
 @NoArgsConstructor
 public class CategorySaveRequest {
+
     private String name;
     private Long parentId;
 }
