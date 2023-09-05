2bbc0d8e70fa0355f075e60816cff6439bee41d8
Choo <choo000407@naver.com>
2023-09-05 22:23:54 +0900

    Feat: Category 전체 목록 조회 시 position 반환 추가

--- src/main/java/kdkd/youre/backend/domain/category/presentation/dto/response/CategoryFindAllResponse.java
+++ src/main/java/kdkd/youre/backend/domain/category/presentation/dto/response/CategoryFindAllResponse.java
@@ -13,6 +13,7 @@ public class CategoryFindAllResponse {
     private Long parent;        // 부모 카테고리 id
     private Boolean droppable;  // 하위에 카테고리 생성 가능한지 여부 (depth 3이면 false, 1 또는 2면 true)
     private String text;        // 카테고리 이름
+    private DataDto data;       // 기타 정보(순서)
 
     public static CategoryFindAllResponse from(Category category) {
 
@@ -21,6 +22,21 @@ public class CategoryFindAllResponse {
                 .parent(category.getParentId())
                 .droppable(category.getDroppable())
                 .text(category.getName())
+                .data(DataDto.from(category.getPosition()))
                 .build();
+    }
+
+    @Getter
+    @Builder
+    private static class DataDto {
+
+        private Long order;     // 카테고리 순서
+
+        private static DataDto from(Long order) {
+
+            return DataDto.builder()
+                    .order(order)
+                    .build();
+        }
     }
 }
