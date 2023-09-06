518a31ac25ca4f8c4b9e84d532c0651f0fdad36a
ChoiSangOk <tkddhr12345@naver.com>
2023-09-06 23:58:42 +0900

    Feat: AccessToken 만료시간 1시간 수정

--- src/main/java/kdkd/youre/backend/global/security/JwtProperties.java
+++ src/main/java/kdkd/youre/backend/global/security/JwtProperties.java
@@ -4,7 +4,7 @@ import java.time.Duration;
 
 public interface JwtProperties {
 
-    Long ACCESS_TOKEN_EXPIRATION_TIME = Duration.ofMinutes(30).toMillis(); // 30분
+    Long ACCESS_TOKEN_EXPIRATION_TIME = Duration.ofMinutes(60).toMillis(); // 60분
     Long REFRESH_TOKEN_EXPIRATION_TIME = Duration.ofDays(14).toMillis();  // 2주
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
