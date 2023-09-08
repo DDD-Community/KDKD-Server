ce32bf1c444db99f796a84f54b16603852f6db63
ChooSeoYeon <choo000407@naver.com>
2023-09-08 13:13:06 +0900

    Refactor: urlKeywordExpression 메서드 분리

--- src/main/java/kdkd/youre/backend/domain/url/domain/repository/UrlRepositoryImpl.java
+++ src/main/java/kdkd/youre/backend/domain/url/domain/repository/UrlRepositoryImpl.java
@@ -4,10 +4,12 @@ import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+
 import kdkd.youre.backend.domain.member.domain.Member;
 import kdkd.youre.backend.domain.url.domain.Url;
 import kdkd.youre.backend.domain.url.presentation.dto.request.UrlFindAllParam;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -19,60 +21,69 @@ import static kdkd.youre.backend.domain.url.domain.QUrl.url;
 @RequiredArgsConstructor
 public class UrlRepositoryImpl implements UrlCustomRepository {
 
-    private final JPAQueryFactory queryFactory;
+	private final JPAQueryFactory queryFactory;
 
-    private BooleanExpression categoryIdExpression(Long categoryId) {
-        if (categoryId == null) {
-            return null;
-        }
-        return url.category.id.eq(categoryId);
-    }
+	private BooleanExpression urlKeywordExpression(String urlKeyword) {
+		return url.name.like("%" + urlKeyword + "%")
+			.or(tag.name.like("%" + urlKeyword + "%"))
+			.or(url.memo.like("%" + urlKeyword + "%"));
+	}
 
-    private BooleanExpression isWatchedBooleanExpression(Boolean isWatched) {
-        if (isWatched != null) {
-            return url.isWatchedLater.eq(isWatched);
-        }
-        return null;
-    }
+	private BooleanExpression categoryIdExpression(Long categoryId) {
+		if (categoryId == null) {
+			return null;
+		}
+		return url.category.id.eq(categoryId);
+	}
 
-    private OrderSpecifier<?> createOrderSpecifier(String order) {
-        if (order.equals("asc")) return url.createdAt.asc();
-        return url.createdAt.desc();
-    }
+	private BooleanExpression isWatchedBooleanExpression(Boolean isWatched) {
+		if (isWatched != null) {
+			return url.isWatchedLater.eq(isWatched);
+		}
+		return null;
+	}
 
-    @Override
-    public List<Url> findBySearchWord(Member member, UrlFindAllParam params, Pageable pageable) {
+	private OrderSpecifier<?> createOrderSpecifier(String order) {
+		if (order.equals("asc"))
+			return url.createdAt.asc();
+		return url.createdAt.desc();
+	}
 
-        OrderSpecifier<?> orderSpecifier = createOrderSpecifier(params.getOrder());
-        BooleanExpression isWatchedExpression = isWatchedBooleanExpression(params.getIsWatched());
-        BooleanExpression categoryIdExpression = categoryIdExpression(params.getCategoryId());
+	@Override
+	public List<Url> findBySearchWord(Member member, UrlFindAllParam params, Pageable pageable) {
 
-        JPAQuery<Url> query = queryFactory
-                .selectDistinct(url)
-                .from(url)
-                .join(tag)
-                .on(url.id.eq(tag.url.id))
-                .join(category)
-                .on(url.category.id.eq(category.id))
-                .join(category.member)
-                .on(category.member.eq(member))
-                .where(url.name.like("%" + params.getUrlKeyword() + "%")
-                        .or(tag.name.like("%" + params.getUrlKeyword() + "%"))
-                        .or(url.memo.like("%" + params.getUrlKeyword() + "%"))
-                        .and(category.member.eq(member)));
+		OrderSpecifier<?> orderSpecifier = createOrderSpecifier(params.getOrder());
+		BooleanExpression urlKeywordExpression = urlKeywordExpression(params.getUrlKeyword());
+		BooleanExpression categoryIdExpression = categoryIdExpression(params.getCategoryId());
+		BooleanExpression isWatchedExpression = isWatchedBooleanExpression(params.getIsWatched());
 
-        if (categoryIdExpression != null) {
-            query = query.where(categoryIdExpression);
-        }
+		JPAQuery<Url> query = queryFactory
+			.selectDistinct(url)
+			.from(url)
+			.join(tag)
+			.on(url.id.eq(tag.url.id))
+			.join(category)
+			.on(url.category.id.eq(category.id))
+			.join(category.member)
+			.on(category.member.eq(member))
+			.where(category.member.eq(member));
 
-        if (isWatchedExpression != null) {
-            query = query.where(isWatchedExpression);
-        }
+		if (urlKeywordExpression != null) {
+			query = query.where(urlKeywordExpression);
+		}
 
-        return query
-                .orderBy(orderSpecifier)
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetch();
-    }
+		if (categoryIdExpression != null) {
+			query = query.where(categoryIdExpression);
+		}
+
+		if (isWatchedExpression != null) {
+			query = query.where(isWatchedExpression);
+		}
+
+		return query
+			.orderBy(orderSpecifier)
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.fetch();
+	}
 }
